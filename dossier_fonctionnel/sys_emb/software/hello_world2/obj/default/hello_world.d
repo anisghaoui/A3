715f0fa17ad5701dd18e0f2722ae62494727b393
Anis Ghaoui <mohamedanisghaoui@mailfence.com>
2020-01-30 19:19:40 +0100

    ajouté : projet fonctionnel + rédaction

---
+++ dossier_fonctionnel/sys_emb/software/hello_world2/obj/default/hello_world.d
@@ -0,0 +1,6 @@
+obj/default/hello_world.o: hello_world.c ../hello_world2_bsp/system.h \
+  ../hello_world2_bsp/linker.h
+
+../hello_world2_bsp/system.h:
+
+../hello_world2_bsp/linker.h:
---
+++ dossier_fonctionnel/sys_emb/software/helo/obj/default/hello_world.d
@@ -0,0 +1,35 @@
+obj/default/hello_world.o: hello_world.c ../hello_world2_bsp/system.h \
+  ../hello_world2_bsp/linker.h ../helo_bsp//HAL/inc/sys/alt_timestamp.h \
+  ../helo_bsp//HAL/inc/alt_types.h \
+  ../helo_bsp//drivers/inc/altera_avalon_timer.h \
+  ../helo_bsp//HAL/inc/alt_types.h ../helo_bsp//HAL/inc/sys/alt_dev.h \
+  ../helo_bsp/system.h ../helo_bsp//HAL/inc/sys/alt_llist.h \
+  ../helo_bsp//HAL/inc/priv/alt_dev_llist.h \
+  ../helo_bsp//HAL/inc/sys/alt_llist.h ../helo_bsp//HAL/inc/alt_types.h \
+  ../helo_bsp//HAL/inc/sys/alt_warning.h
+
+../hello_world2_bsp/system.h:
+
+../hello_world2_bsp/linker.h:
+
+../helo_bsp//HAL/inc/sys/alt_timestamp.h:
+
+../helo_bsp//HAL/inc/alt_types.h:
+
+../helo_bsp//drivers/inc/altera_avalon_timer.h:
+
+../helo_bsp//HAL/inc/alt_types.h:
+
+../helo_bsp//HAL/inc/sys/alt_dev.h:
+
+../helo_bsp/system.h:
+
+../helo_bsp//HAL/inc/sys/alt_llist.h:
+
+../helo_bsp//HAL/inc/priv/alt_dev_llist.h:
+
+../helo_bsp//HAL/inc/sys/alt_llist.h:
+
+../helo_bsp//HAL/inc/alt_types.h:
+
+../helo_bsp//HAL/inc/sys/alt_warning.h:
